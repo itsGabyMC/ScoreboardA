6efad2df663bfa5c062e8c004927ca68f20cb56c
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-09-02 22:14:15 -0300

    update teams implementation

--- src/dev/_2lstudios/scoreboard/hooks/TeamsHook.java
+++ src/dev/_2lstudios/scoreboard/hooks/TeamsHook.java
@@ -1,12 +1,12 @@
 package dev._2lstudios.scoreboard.hooks;
 
-import dev._2lstudios.teams.team.TPlayer;
 import org.bukkit.entity.Player;
 import dev._2lstudios.teams.Teams;
-import dev._2lstudios.teams.managers.TPlayerManager;
+import dev._2lstudios.teams.managers.TeamPlayerManager;
+import dev._2lstudios.teams.team.TeamPlayer;
 
 public class TeamsHook {
-    private TPlayerManager tPlayerManager;
+    private TeamPlayerManager teamPlayerManager;
     private boolean hooked;
 
     public TeamsHook() {
@@ -14,14 +14,14 @@ public class TeamsHook {
     }
 
     public void hook() {
-        this.tPlayerManager = Teams.getTeamsManager().getTPlayerManager();
+        this.teamPlayerManager = Teams.getTeamsManager().getTeamPlayerManager();
         this.hooked = true;
     }
 
     public boolean isSameTeam(final Player player1, final Player player2) {
         if (this.hooked) {
-            final TPlayer tPlayer1 = this.tPlayerManager.getPlayer(player1.getName());
-            final TPlayer tPlayer2 = this.tPlayerManager.getPlayer(player2.getName());
+            final TeamPlayer tPlayer1 = this.teamPlayerManager.getPlayer(player1.getName());
+            final TeamPlayer tPlayer2 = this.teamPlayerManager.getPlayer(player2.getName());
             if (tPlayer1 != null && tPlayer2 != null) {
                 final String team1 = tPlayer1.getTeam();
                 final String team2 = tPlayer2.getTeam();
